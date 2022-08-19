510665d1a1fadf84eda014e0f7b25443e1a90163
ybiblow <ybiblow@gmail.com>
2022-08-19 20:08:46 +0300

    Uploading project

---
+++ app/src/main/java/dev/jacob_ba/timetoswim/Student.java
@@ -0,0 +1,7 @@
+package dev.jacob_ba.timetoswim;
+
+public class Student {
+    private String name;
+    private String lastName;
+
+}
---
+++ app/src/main/java/dev/jacob_ba/timetoswim/Teacher.java
@@ -0,0 +1,6 @@
+package dev.jacob_ba.timetoswim;
+
+public class Teacher {
+    private String name;
+    private String lastName;
+}
